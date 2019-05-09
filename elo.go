7ebe769ea0d0f4e13332964d57d13f59388e6ba7
Ethan Madison <ethan@ethanmad.com>
2019-05-09 02:11:22 -0400

    Add elo.go and elo_test.go
    Supports draws, calculating expected scores, rating deltas. Includes
    minimal Player and Match structs.
    
    100% test coverage.
    
    Signed-off-by: Ethan Madison <ethan@ethanmad.com>

---
+++ elo.go
@@ -0,0 +1,51 @@
+package elo
+
+import (
+	"math"
+)
+
+const (
+	deviation uint    = 400
+	k         uint    = 32
+	winScore  float64 = 1.0
+	drawScore float64 = 0.5
+	loseScore float64 = 0.0
+)
+
+// Player contains the Player's rating.
+// Using a struct to support the future addition of other information.
+type Player struct {
+	rating int
+}
+
+// Match contains two *Players: w is the winner, l is the loser.
+// If draw is set, the order of *Players doesn't matter.
+type Match struct {
+	w, l *Player
+	draw bool
+}
+
+// Updates the player's ratings
+func (m *Match) play() {
+	d := int(m.delta())
+	m.w.rating += d
+	m.l.rating -= d
+}
+
+// ExpectedScore returns the expected score of the Player corresponding to ratingA.
+func ExpectedScore(ratingA, ratingB int) float64 {
+	return 1 / (1 + math.Pow(10, float64((ratingB-ratingA))/float64(deviation)))
+}
+
+// delta returns the absolute value of the rating change each player incurs as a result of the match.
+func (m *Match) delta() uint {
+	expec := ExpectedScore(m.w.rating, m.l.rating)
+	d := 0.0
+	if m.draw {
+		d = drawScore - expec
+	} else {
+		d = winScore - expec
+	}
+	d *= float64(k)
+	return uint(math.Abs(math.Round(d)))
+}
---
+++ elo_test.go
@@ -0,0 +1,95 @@
+package elo
+
+import (
+	"math"
+	"testing"
+)
+
+func TestPlay(t *testing.T) {
+	m := &Match{&Player{rating: 1000}, &Player{rating: 1000}, false}
+	m.play()
+	if m.w.rating != 1016 && m.l.rating != 984 {
+		t.Errorf("expecting winner rating 1016, got %d", m.w.rating)
+	}
+}
+
+func TestExpectedScore(t *testing.T) {
+	for _, d := range provideExpectedScore() {
+		result := ExpectedScore(d.ratingA, d.ratingB)
+		rounded := int(math.Round(result * 100))
+		if rounded != d.score {
+			t.Errorf("expecting %d, got %d", d.score, rounded)
+		}
+	}
+}
+
+type dataExpectedScore struct {
+	ratingA, ratingB int
+	score            int
+}
+
+func provideExpectedScore() []dataExpectedScore {
+	data := []dataExpectedScore{
+		{
+			1000, 1000,
+			50,
+		},
+		{
+			1400, 1100,
+			85,
+		},
+		{
+			1100, 1000,
+			64,
+		},
+		{
+			1000, 1100,
+			36,
+		},
+	}
+	return data
+}
+
+func TestDelta(t *testing.T) {
+	for _, d := range provideDelta() {
+		result := d.m.delta()
+		if result != d.delta {
+			t.Errorf("expecting %d, got %d", d.delta, result)
+		}
+	}
+}
+
+type dataDelta struct {
+	m     Match
+	delta uint
+}
+
+func provideDelta() []dataDelta {
+	data := []dataDelta{
+		{
+			Match{&Player{rating: 1000}, &Player{rating: 1000}, false},
+			16,
+		},
+		{
+			Match{&Player{rating: 1000}, &Player{rating: 1000}, true},
+			0,
+		},
+		{
+			Match{&Player{rating: 1100}, &Player{rating: 1000}, false},
+			12,
+		},
+		{
+			Match{&Player{rating: 1000}, &Player{rating: 1100}, false},
+			20,
+		},
+		{
+			Match{&Player{rating: 1100}, &Player{rating: 1400}, false},
+			27,
+		},
+		{
+			Match{&Player{rating: 1400}, &Player{rating: 1100}, false},
+			5,
+		},
+	}
+	return data
+}
